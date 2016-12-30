5666036f912d48c2c9939fcf892dad3b5c7d505f
miigotu <miigotu@gmail.com>
2016-12-30 13:31:16 -0800

    Update QSickRage.sh

--- shared/QSickRage.sh
+++ shared/QSickRage.sh
@@ -26,9 +26,9 @@ fi
 export PATH=${QPKG_DIR}/${ver}/bin-utils:/Apps/bin:/usr/local/bin:$PATH
 
 CheckQpkgEnabled() { # Is the QPKG enabled? if not exit the script
-  if [ $(/sbin/getcfg ${QPKG_NAME} Enable -u -d FALSE -f /etc/config/qpkg.conf) = UNKNOWN ]; then
-        /sbin/setcfg ${QPKG_NAME} Enable TRUE -f /etc/config/qpkg.conf
-  elif [ $(/sbin/getcfg ${QPKG_NAME} Enable -u -d FALSE -f /etc/config/qpkg.conf) != TRUE ]; then
+  if [ $($CMD_GETCFG ${QPKG_NAME} Enable -u -d FALSE -f /etc/config/qpkg.conf) = UNKNOWN ]; then
+      $CMD_SETCFG ${QPKG_NAME} Enable TRUE -f /etc/config/qpkg.conf
+  elif [ $($CMD_GETCFG ${QPKG_NAME} Enable -u -d FALSE -f /etc/config/qpkg.conf) != TRUE ]; then
       /bin/echo "${QPKG_NAME} is disabled."
       exit 1
   fi
@@ -36,24 +36,23 @@ CheckQpkgEnabled() { # Is the QPKG enabled? if not exit the script
 
 ConfigPython(){ # checks if the daemon exists and will link /usr/bin/python to it
   # python dependency checking
-        VER=0
-        DAEMON="None"
-        for DAEMON2 in /usr/bin/python2.7 /usr/local/bin/python2.7 /opt/bin/python2.7 /Apps/opt/bin/python2.7 /opt/QPython2/bin/python2.7
-        do
-                echo "Looking for $DAEMON2"
-                [ ! -x $DAEMON2 ] && continue
-                VER2=$(expr substr "$(${DAEMON2} -V 2>&1)" 8 8)
-                if [ $VER2 > $VER ]; then
-                        DAEMON=$DAEMON2
-                        VER=$VER2
-                fi
-        done
-        if [ ! -x $DAEMON ]; then
-                /sbin/write_log "Failed to start $QPKG_NAME, Python was not found. Please re-install the Python qpkg." 1
-                exit 1
-        else
-                /bin/echo "Found Python Version ${VER} at ${DAEMON}"
+    VER=0
+    DAEMON="None"
+    for DAEMON2 in /usr/bin/python2.7 /usr/local/bin/python2.7 /opt/bin/python2.7 /Apps/opt/bin/python2.7 /opt/QPython2/bin/python2.7; do
+        echo "Looking for $DAEMON2"
+        [ ! -x $DAEMON2 ] && continue
+        VER2=$(expr substr "$(${DAEMON2} -V 2>&1)" 8 8)
+        if [ $VER2 > $VER ]; then
+            DAEMON=$DAEMON2
+            VER=$VER2
         fi
+    done
+    if [ ! -x $DAEMON ]; then
+        log_error "Failed to start $QPKG_NAME, Python was not found. Please re-install the Python qpkg." 1
+        exit 1
+    else
+        log "Found Python Version ${VER} at ${DAEMON}"
+    fi
 }
 
 CheckQpkgRunning() { # Is the QPKG already running? if so, exit the script
@@ -61,7 +60,7 @@ CheckQpkgRunning() { # Is the QPKG already running? if so, exit the script
     # grab pid from pid file
     Pid=$(/bin/cat $PID_FILE)
     if [ -d /proc/$Pid ]; then
-      /bin/echo " $QPKG_NAME is already running"
+      log "$QPKG_NAME is already running"
       exit 1
     fi
   fi
@@ -69,57 +68,55 @@ CheckQpkgRunning() { # Is the QPKG already running? if so, exit the script
 }
 
 StartQpkg(){
-  /bin/echo "Starting $QPKG_NAME"
-  cd $QPKG_DIR/$QPKG_NAME
-  PATH=${PATH} ${DAEMON} ${DAEMON_OPTS}
+    log "Starting $QPKG_NAME"
+    cd $QPKG_DIR/$QPKG_NAME
+    PATH=${PATH} ${DAEMON} ${DAEMON_OPTS}
 }
 
 ShutdownQPKG() { # kills a proces based on a PID in a given PID file
-  /bin/echo "Shutting down ${QPKG_NAME}... "
-  if [ -f $PID_FILE ]; then
-    # grab pid from pid file
-    Pid=$(/bin/cat $PID_FILE)
-    i=0
-    /bin/kill $Pid
-    /bin/echo -n " Waiting for ${QPKG_NAME} to shut down: "
-    while [ -d /proc/$Pid ]; do
-      sleep 1
-      let i+=1
-      /bin/echo -n "$i, "
-      if [ $i = 45 ]; then
-        /bin/echo " Tired of waiting, killing ${QPKG_NAME} now"
-        /bin/kill -9 $Pid
-        /bin/rm -f $PID_FILE
-        exit 1
-      fi
-    done
-    /bin/rm -f $PID_FILE
-    /bin/echo "Done"
-  else
-    /bin/echo "${QPKG_NAME} is not running?"
-  fi
+    log "Shutting down ${QPKG_NAME}... "
+    if [ -f $PID_FILE ]; then
+        # grab pid from pid file
+        Pid=$($CMD_CAT $PID_FILE)
+        i=0
+        /bin/kill $Pid
+        log " Waiting for ${QPKG_NAME} to shut down: "
+        while [ -d /proc/$Pid ]; do
+            sleep 1
+            let i+=1
+            /bin/echo -n "$i, "
+            if [ $i = 45 ]; then
+                log " Tired of waiting, killing ${QPKG_NAME} now"
+                /bin/kill -9 $Pid
+                /bin/rm -f $PID_FILE
+                exit 1
+            fi
+        done
+        $CMD_RM -f $PID_FILE
+        log "Done"
+    else
+        log "${QPKG_NAME} is not running?"
+    fi
 }
 
 case "$1" in
-  start)
-  CheckQpkgEnabled # Check if the QPKG is enabled, else exit
-  /bin/echo "$QPKG_NAME prestartup checks..."
-  CheckQpkgRunning # Check if the QPKG is not running, else exit
-  ConfigPython   # Check for Python, exit if not found
-  StartQpkg    # Finally Start the qpkg
-
-  ;;
+    start)
+        CheckQpkgEnabled # Check if the QPKG is enabled, else exit
+        CheckQpkgRunning # Check if the QPKG is not running, else exit
+        ConfigPython   # Check for Python, exit if not found
+        StartQpkg    # Finally Start the qpkg
+        ;;
   stop)
-    ShutdownQPKG
-  ;;
+        ShutdownQPKG
+        ;;
   restart)
-  echo "Restarting $QPKG_NAME"
-  $0 stop
-  $0 start
-  ;;
+        echo "Restarting $QPKG_NAME"
+        $0 stop
+        $0 start
+        ;;
   *)
-  N=/etc/init.d/$QPKG_NAME.sh
-  echo "Usage: $N {start|stop|restart}" >&2
-  exit 1
-  ;;
+        N=/etc/init.d/$QPKG_NAME.sh
+        $CMD_ECHO "Usage: $N {start|stop|restart}" >&2
+        exit 1
+        ;;
 esac
